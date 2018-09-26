fb0d324db8ad7fa0548aff00db45201f3cc9f9ba
Tom Hsu <thomaschsu@gmail.com>
2018-09-26 15:48:18 -0400

    Modified some css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,8 +5,8 @@ const Nav = props => <nav>
     <div className="nav-wrapper blue">
     <a href="/" class="brand-logo">{props.title}</a>
     <ul id="nav-mobile" class="right">
-        <li>Top Score: {props.topScore}</li>
-        <li>Score: {props.score}</li>
+        <li className="nav-score">Top Score: {props.topScore}</li>
+        <li className="nav-score">Score: {props.score}</li>
     </ul>
     </div>
     </nav>;
