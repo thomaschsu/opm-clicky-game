bca69916c11c4c444815f59e12ced4e4057dc840
Tom Hsu <thomaschsu@gmail.com>
2018-09-26 15:56:15 -0400

    Modified some more styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   // Set state
   state = {
     friends,
-    score: 0
+    score: 0,
+    topScore: 0
   };
 
   click = () => {
@@ -22,8 +23,11 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Nav title="One Punch Man Clicky Game" />
-        <Title>Click on an image to earn 1 point, but don't click on that same image or you lose!</Title>
+        <Nav title="One Punch Man - Clicky Game"
+        score={this.state.score}
+        topScore={this.state.topScore}
+        />
+        <Title>The goal is to memorize which images you have already clicked on. Click on any image to earn a point, but don't click on that same image or the game will end and restart!</Title>
         <CardWrapper>
           {this.state.friends.map(friend => (
             <FriendCard
