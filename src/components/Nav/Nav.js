ba466d00f98f2fa9555e5fa171349430aef394f9
Tom Hsu <thomaschsu@gmail.com>
2018-09-27 11:40:44 -0400

    Small changes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.css";
 const Nav = props => <nav>
     <div className="nav-wrapper blue">
     <a href="/" className="brand-logo">{props.title}</a>
-    <ul id="nav-mobile" class="right">
+    <ul id="nav-mobile" className="right">
         <li className="nav-score">Score: {props.score}</li>
     </ul>
     </div>
